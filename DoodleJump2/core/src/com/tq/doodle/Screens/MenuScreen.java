6f919d3b534a65326519965f94ea9e39c2829dd4
Ines Ferreira <inesferreira789@gmail.com>
2016-06-03 03:34:00 +0100

    Menu working on Mysterio's phone eheh

--- DoodleJump2/core/src/com/tq/doodle/Screens/MenuScreen.java
+++ DoodleJump2/core/src/com/tq/doodle/Screens/MenuScreen.java
@@ -23,42 +23,45 @@ public class MenuScreen implements Screen {
     private Texture scoresBtn;
     private Texture menudoodle;
     private Texture ovni;
+    private OrthographicCamera menucam;
 
-    public static int title_width = Gdx.graphics.getWidth() -300;
-    public static int title_height = 180;
-    public static int btn_width = 300;
-    public static int btn_height = 100;
-    public static int doodle_width = 400;
-    public static int doodle_height = 400;
-    public static int play_width = 400;
-    public static int play_height = 150;
+    public static int title_width = Gdx.graphics.getWidth() - 100;
+    public static int title_height = 140;
 
-    public MenuScreen(DoodleJump game){
+    public static int btn_width = 150;
+    public static int btn_height = 70;
+
+    public static int doodle_width = 200;
+    public static int doodle_height = 200;
+
+    public static int play_width = 170;
+    public static int play_height = 75;
+
+    public MenuScreen(DoodleJump game) {
         this.game = game;
         this.background = new Texture("background.png");
         this.title = new Texture("title.png");
         this.optionsBtn = new Texture("options.png");
         this.scoresBtn = new Texture("scores.png");
         menudoodle = new Texture("menu.png");
-        //ovni = new Texture("ovni.png");
+        menucam = new OrthographicCamera();
 
         //Buttonplay variables
         this.playBtn = new Texture("play.png");
 
     }
 
-    public void handleInput(float dt){
+    public void handleInput(float dt) {
 
-        if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+        if (Gdx.input.isTouched()) {
             int cursor_x = Gdx.input.getX();
             int cursor_y = Gdx.input.getY();
-            if (cursor_x > Gdx.graphics.getWidth()/ 2 - play_width /2 && cursor_x < Gdx.graphics.getWidth()/2 + play_width/2){
-                if(cursor_y < Gdx.graphics.getHeight()/2+400 && cursor_y > Gdx.graphics.getHeight()/2+400-play_height){
+            if (cursor_x > Gdx.graphics.getWidth() / 2 - play_width / 2 && cursor_x < Gdx.graphics.getWidth() / 2 + play_width / 2) {
+                if (cursor_y > Gdx.graphics.getHeight() / 2 + 195 && cursor_y < Gdx.graphics.getHeight() / 2 + 195 + play_height) {
                     game.setScreen(new PlayScreen(game));
                 }
             }
         }
-
     }
 
     public void update(float dt){
@@ -77,12 +80,11 @@ public class MenuScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
         game.batch.draw(background,0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        game.batch.draw(title,Gdx.graphics.getWidth() / 2 - title_width /2, Gdx.graphics.getHeight() / 2 + 400, title_width, title_height);
-        game.batch.draw(playBtn,Gdx.graphics.getWidth() /2 - play_width/2,Gdx.graphics.getHeight() /2 - 400, play_width, play_height);
-        game.batch.draw(optionsBtn, Gdx.graphics.getWidth()/4 - btn_width/4, Gdx.graphics.getHeight()/2 -600, btn_width, btn_height);
-        game.batch.draw(scoresBtn, Gdx.graphics.getWidth()/2 + optionsBtn.getWidth()/2, Gdx.graphics.getHeight() / 2 -600, btn_width, btn_height);
+        game.batch.draw(title,Gdx.graphics.getWidth() / 2 - title_width /2, Gdx.graphics.getHeight() / 2 + 200, title_width, title_height);
+        game.batch.draw(playBtn,Gdx.graphics.getWidth() /2 - play_width/2,Gdx.graphics.getHeight() /2 - 250, play_width, play_height);
+        game.batch.draw(optionsBtn, Gdx.graphics.getWidth()/4 - btn_width/4, Gdx.graphics.getHeight()/2 - 350, btn_width, btn_height);
+        game.batch.draw(scoresBtn, Gdx.graphics.getWidth()/2 + optionsBtn.getWidth()/2 -10, Gdx.graphics.getHeight() / 2 - 350, btn_width, btn_height);
         game.batch.draw(menudoodle, Gdx.graphics.getWidth()/2 - doodle_width/2, Gdx.graphics.getHeight()/2 - 100, doodle_width, doodle_height);
-        //game.batch.draw(ovni, Gdx.graphics.getWidth()/4 - 100, Gdx.graphics.getHeight()/2 + 600, 150, 150);
         game.batch.end();
     }
 
