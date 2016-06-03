fa2e6656134c9124403f780f4fa0feb4601f9d17
Ines Ferreira <inesferreira789@gmail.com>
2016-06-03 15:36:03 +0100

    Design of PauseScreen

--- DoodleJump2/core/src/com/tq/doodle/Screens/PauseScreen.java
+++ DoodleJump2/core/src/com/tq/doodle/Screens/PauseScreen.java
@@ -13,10 +13,18 @@ public class PauseScreen implements Screen {
 
     private DoodleJump game;
     private Texture background_pause;
+    private Texture menuBtn;
+    private Texture resumeBtn;
+
+    public static final int btn_width = 140;
+    public static final int btn_height = 60;
+
 
     public PauseScreen (DoodleJump game){
         this.game = game;
         this.background_pause = new Texture("background_pause.png");
+        this.menuBtn = new Texture("menubtn.png");
+        this.resumeBtn = new Texture("resume.png");
     }
 
     public void update(float dt){
@@ -25,6 +33,10 @@ public class PauseScreen implements Screen {
     }
 
     public void handleInput(float dt){
+
+        //Voltar para o PlayScreen
+
+        //Ir para o Menu Inicial
 
     }
 
@@ -38,7 +50,9 @@ public class PauseScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
-        game.batch.draw(background_pause, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        game.batch.draw(background_pause, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //desenhar background
+        game.batch.draw(menuBtn, Gdx.graphics.getWidth()/2 - btn_width/2 - 30, Gdx.graphics.getHeight()/2 -350 , btn_width, btn_height);
+        game.batch.draw(resumeBtn, Gdx.graphics.getWidth()/2 - btn_width/2 - 30, Gdx.graphics.getHeight()/2 - 230, btn_width, btn_height);
         game.batch.end();
     }
 
@@ -60,5 +74,7 @@ public class PauseScreen implements Screen {
 
     @Override
     public void dispose() {
+        background_pause.dispose();
+        menuBtn.dispose();
     }
 }
